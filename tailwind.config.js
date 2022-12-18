eb11d1d03e217ae7ac3a86ee521918831bd8c33b
David Dragovacz <david.dragovacz@deliveroo.co.uk>
2022-12-18 15:05:45 +0000

    Add plugin to `tailwind.config`

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,9 +3,10 @@ module.exports = {
   content: [
     "./pages/**/*.{js,ts,jsx,tsx}",
     "./components/**/*.{js,ts,jsx,tsx}",
+    "./contexts/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
     extend: {},
   },
-  plugins: [],
+  plugins: [require('@headlessui/tailwindcss')],
 };
