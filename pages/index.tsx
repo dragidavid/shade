8033f6e56c5cba77db8c3ac34a3423b63c5155fd
David Dragovacz <david.dragovacz@deliveroo.co.uk>
2023-01-01 21:27:29 +0000

    remove unused api route, add comments & fix schema

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,8 +2,8 @@ import Code from "components/Code";
 import Settings from "components/Settings";
 import Dashboard from "components/Dashboard";
 
-import { getServerSession } from "lib/auth";
 import { exists } from "lib/exists";
+import { getServerSession } from "lib/auth";
 
 import type { GetServerSidePropsContext } from "next";
 import type { Session } from "next-auth";
@@ -13,10 +13,20 @@ interface HomePageProps {
 }
 
 export default function HomePage({ s }: HomePageProps) {
+  /**
+   * If the user is authenticated, show the `Dashboard` component.
+   * In this "mode" the user can do CRUD operations on their snippets.
+   *
+   * When the user clicks `New`, the site will redirect to a new page with a new `id` in the URL.
+   */
   if (exists(s)) {
     return <Dashboard />;
   }
 
+  /**
+   * If the user is not authenticated, just show a the `Code` and `Settings` components.
+   * In this "mode" the user can't save any snippets in the database. They can only share it as a screenshot.
+   */
   return (
     <>
       <Code />
