1e06b031f9e97054283599120c99b1f5819b879e
David Dragovacz <david.dragovacz@deliveroo.co.uk>
2023-01-01 18:30:15 +0000

    render different things based on session

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,22 @@
 import Code from "components/Code";
 import Settings from "components/Settings";
+import Dashboard from "components/Dashboard";
 
-export default function Home() {
+import { getServerSession } from "lib/auth";
+import { exists } from "lib/exists";
+
+import type { GetServerSidePropsContext } from "next";
+import type { Session } from "next-auth";
+
+interface HomePageProps {
+  s: Session;
+}
+
+export default function HomePage({ s }: HomePageProps) {
+  if (exists(s)) {
+    return <Dashboard />;
+  }
+
   return (
     <>
       <Code />
@@ -9,4 +24,14 @@ export default function Home() {
       <Settings />
     </>
   );
+}
+
+export async function getServerSideProps(context: GetServerSidePropsContext) {
+  const session = await getServerSession(context.req, context.res);
+
+  return {
+    props: {
+      s: session,
+    },
+  };
 }
