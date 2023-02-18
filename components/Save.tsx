a7e4d1521e303e20b265ee721f30affc02439089
David Dragovacz <david.dragovacz@deliveroo.co.uk>
2023-02-18 12:55:35 +0000

    handle errors in `Save` and change api route

--- components/Save.tsx
+++ components/Save.tsx
@@ -7,46 +7,51 @@ import isEqual from "lodash.isequal";
 import { useStateContext } from "contexts/State";
 
 import { exists } from "lib/exists";
+import { fetcher } from "lib/fetcher";
 
 import type { State } from "lib/types";
 
 export default function Save() {
   const prevStateRef = useRef<State | null>(null);
   const shouldUpdate = useRef(false);
+  const hasFailed = useRef(false);
+  const cancellationTokenRef = useRef({ isCancelled: false });
 
   const { state, saveState, setSaveState } = useStateContext();
 
   const { trigger } = useSWRMutation(
     "/api/snippets/update",
     (url, { arg }) =>
-      fetch(url, {
+      fetcher(url, {
         method: "PATCH",
         body: JSON.stringify(arg),
         headers: { "Content-Type": "application/json" },
       }),
     {
-      onSuccess: async (data) => {
-        if (!data.ok) {
-          return;
-        }
-
-        setSaveState("SUCCESS");
-      },
-      onError: (error) => {
-        // TODO - Handle error
-        console.log(error);
-
-        setSaveState("ERROR");
-      },
+      revalidate: false,
     }
   );
 
   const debouncedSave = useCallback(
-    debounce(async (changes) => {
+    debounce(async (changes, cancellationToken) => {
+      if (cancellationToken.isCancelled) {
+        return;
+      }
+
       if (!isEqual(state, changes)) {
         if (saveState !== "PENDING") setSaveState("PENDING");
 
-        await trigger(changes);
+        try {
+          await trigger(changes);
+
+          if (!cancellationToken.isCancelled) {
+            setSaveState("SUCCESS");
+          }
+        } catch (e) {
+          if (!cancellationToken.isCancelled) {
+            setSaveState("ERROR");
+          }
+        }
       } else {
         setSaveState("IDLE");
       }
@@ -54,11 +59,12 @@ export default function Save() {
     [state.id, shouldUpdate.current]
   );
 
-  /**
-   * 1. If the save was successful, update the previous state to the current state
-   * 2. Flip the shouldUpdate ref so that the debouncedSave function will run
-   * 3. Reset the save state to IDLE
-   */
+  useEffect(() => {
+    return () => {
+      cancellationTokenRef.current.isCancelled = true;
+    };
+  }, []);
+
   useEffect(() => {
     if (saveState === "SUCCESS") {
       prevStateRef.current = state;
@@ -66,11 +72,14 @@ export default function Save() {
 
       setSaveState("IDLE");
     }
+
+    if (saveState === "ERROR") {
+      hasFailed.current = !hasFailed.current;
+
+      setSaveState("IDLE");
+    }
   }, [state, saveState]);
 
-  /**
-   * This should only run if the state id changes which means we're on a new snippet
-   */
   useEffect(() => {
     prevStateRef.current = state;
 
@@ -78,24 +87,29 @@ export default function Save() {
   }, [state.id]);
 
   useEffect(() => {
-    /**
-     * If the save state is pending and the previous state is equal to the current state,
-     * cancel the debouncedSave function and reset the save state to IDLE
-     */
+    if (hasFailed.current) {
+      hasFailed.current = false;
+    }
+  }, [state]);
+
+  useEffect(() => {
     if (saveState === "PENDING" && isEqual(prevStateRef.current, state)) {
       debouncedSave.cancel();
 
       setSaveState("IDLE");
     }
 
-    /**
-     * If the state id exists and the previous state is not equal to the current state,
-     * set the save state to PENDING and run the debouncedSave function
-     */
-    if (exists(state.id) && !isEqual(prevStateRef.current, state)) {
+    if (
+      exists(state.id) &&
+      !isEqual(prevStateRef.current, state) &&
+      !hasFailed.current
+    ) {
+      cancellationTokenRef.current.isCancelled = true;
+      cancellationTokenRef.current = { isCancelled: false };
+
       setSaveState("PENDING");
 
-      debouncedSave(state);
+      debouncedSave(state, cancellationTokenRef.current);
     }
   }, [state, saveState]);
 
--- pages/api/snippets/update.ts
+++ pages/api/snippets/update.ts
@@ -1,36 +1,32 @@
 import prisma from "lib/prisma";
 import { withAuthentication } from "lib/auth";
-import { exists } from "lib/exists";
 
 import type { NextApiRequest, NextApiResponse } from "next";
 
 async function handler(req: NextApiRequest, res: NextApiResponse) {
-  const snippet = await prisma.snippet.update({
-    where: {
-      id: req.body.id,
-    },
-    data: {
-      title: req.body.title,
-      code: req.body.code,
-      settings: {
-        language: req.body.language.id,
-        theme: req.body.theme.id,
-        fontStyle: req.body.fontStyle.id,
-        lineNumbers: req.body.lineNumbers,
-        padding: req.body.padding.id,
+  try {
+    const updatedSnippet = await prisma.snippet.update({
+      where: {
+        id: req.body.id,
       },
-      updatedAt: new Date().toISOString(),
-    },
-  });
+      data: {
+        title: req.body.title,
+        code: req.body.code,
+        settings: {
+          language: req.body.language.id,
+          theme: req.body.theme.id,
+          fontStyle: req.body.fontStyle.id,
+          lineNumbers: req.body.lineNumbers,
+          padding: req.body.padding.id,
+        },
+        updatedAt: new Date().toISOString(),
+      },
+    });
 
-  // TODO - capture the error on the client side and display an error message
-  if (!exists(snippet.id)) {
-    res.status(400).json({ error: "Unable to save changes to this snippet." });
-
-    return;
+    res.status(200).json(updatedSnippet);
+  } catch (e) {
+    res.status(500).send({ message: "Unable to update the snippet." });
   }
-
-  res.status(200).json(req.body);
 }
 
 export default withAuthentication(handler);
