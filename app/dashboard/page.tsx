2b40b69562e982270fee1b1b08858f5780ee61d1
David Dragovacz <david.dragovacz@deliveroo.co.uk>
2023-03-30 18:25:50 +0100

    remove `layout` from Dashboard

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,5 +1,9 @@
+import { redirect } from "next/navigation";
+
+import Button from "components/Dashboard/Button";
 import Snippets from "components/Dashboard/Snippets";
 
+import { cn } from "lib/cn";
 import { prisma } from "lib/prisma";
 import { getSession } from "lib/auth";
 import { serialize } from "lib/serialize";
@@ -15,7 +19,28 @@ async function getSnippets(userId: string) {
 export default async function Page() {
   const session = await getSession();
 
+  if (!session) {
+    redirect("/");
+  }
+
   const snippets = await getSnippets(session.user.id);
 
-  return <Snippets snippets={serialize(snippets)} />;
+  const shouldButtonBeDisabled = snippets.length >= 10;
+
+  return (
+    <div
+      className={cn(
+        "flex w-[576px] flex-col gap-6 rounded-xl p-5",
+        "border border-white/20 bg-black shadow-xl shadow-black/40"
+      )}
+    >
+      <div className={cn("flex w-full items-center justify-between")}>
+        <h2 className={cn("text-xl font-extrabold")}>Snippets</h2>
+
+        <Button isDisabled={shouldButtonBeDisabled} />
+      </div>
+
+      <Snippets snippets={serialize(snippets)} />
+    </div>
+  );
 }
