9650045e1c0d6723c377d9e8cfd2b66f7d3e10cf
David Dragovacz <david.dragovacz@deliveroo.co.uk>
2023-04-10 16:14:27 +0100

    not sure why was this removed 🤔

--- app/layout.tsx
+++ app/layout.tsx
@@ -24,22 +24,28 @@ export const metadata: Metadata = {
 
 const inter = Inter({
   variable: "--font-inter",
+  subsets: ["latin"],
 });
 const firaCode = Fira_Code({
   variable: "--font-fira-code",
+  subsets: ["latin"],
 });
 const jetBrainsMono = JetBrains_Mono({
   variable: "--font-jetbrains-mono",
+  subsets: ["latin"],
 });
 const inconsolata = Inconsolata({
   variable: "--font-inconsolata",
+  subsets: ["latin"],
 });
 const sourceCodePro = Source_Code_Pro({
   variable: "--font-source-code-pro",
+  subsets: ["latin"],
 });
 const ibmPlexMono = IBM_Plex_Mono({
   weight: "400",
   variable: "--font-ibm-plex-mono",
+  subsets: ["latin"],
 });
 
 export default function RootLayout({
--- next.config.js
+++ next.config.js
@@ -2,12 +2,6 @@
 const nextConfig = {
   experimental: {
     appDir: true,
-    fontLoaders: [
-      {
-        loader: "next/font/google",
-        options: { subsets: ["latin"] },
-      },
-    ],
   },
   reactStrictMode: true,
   swcMinify: true,
