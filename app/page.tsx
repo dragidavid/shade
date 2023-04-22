7db61d92e1fdb6cf0fca331ce09ca25e644149a6
David Dragovacz <david.dragovacz@deliveroo.co.uk>
2023-04-22 20:44:57 +0100

    update metadata

--- app/page.tsx
+++ app/page.tsx
@@ -9,10 +9,35 @@ import type { Metadata } from "next";
 export const metadata: Metadata = {
   title: "shade - Share some code",
   description: "Yet another code sharing app...",
-  twitter: {
-    card: "summary_large_image",
+  openGraph: {
     title: "shade - Share some code",
     description: "Yet another code sharing app...",
+    url: "https://shade.dragi.me",
+    siteName: "shade - Share some code",
+    images: [
+      {
+        url: "https://shade.dragi.me/og.png",
+        width: 1200,
+        height: 600,
+      },
+    ],
+    locale: "en-US",
+    type: "website",
+  },
+  robots: {
+    index: true,
+    follow: true,
+    googleBot: {
+      index: true,
+      follow: true,
+      "max-video-preview": -1,
+      "max-image-preview": "large",
+      "max-snippet": -1,
+    },
+  },
+  twitter: {
+    card: "summary_large_image",
+    title: "shade",
     creator: "@dragidavid",
   },
   metadataBase: new URL("https://shade.dragi.me"),
