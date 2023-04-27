d2baa3102aa7ac4935fa0a3b6dc44953597d3229
David Dragovacz <david.dragovacz@deliveroo.co.uk>
2023-04-27 21:15:04 +0100

    add `and` part in between kbd

--- components/ui/Kbd.tsx
+++ components/ui/Kbd.tsx
@@ -1,18 +1,23 @@
+import { Fragment } from "react";
+
 import { cn } from "lib/cn";
 
 export default function Kbd({ keys }: { keys: string[] }) {
   return (
     <div className={cn("flex items-center gap-1")}>
       {keys.map((key) => (
-        <kbd
-          key={key}
-          className={cn(
-            "rounded-[4px] py-0.5 px-1.5 font-sans font-medium",
-            "bg-white/20"
-          )}
-        >
-          {key}
-        </kbd>
+        <Fragment key={key}>
+          <kbd
+            className={cn(
+              "flex h-6 items-center justify-center rounded-[4px] px-2 font-medium",
+              "bg-white/20"
+            )}
+          >
+            {key}
+          </kbd>
+
+          {key === "hover" && <span>and</span>}
+        </Fragment>
       ))}
     </div>
   );
