2b69d6c45a48234718219310117a29120ae47f52
David Dragovacz <david.dragovacz@deliveroo.co.uk>
2023-04-29 13:16:53 +0100

    prevent default behaviour (opens devtools by default)

--- components/Settings/Actions.tsx
+++ components/Settings/Actions.tsx
@@ -52,6 +52,9 @@ export default function Actions() {
       isDisabled: sessionStatus === "unauthenticated",
       hotkey: {
         key: "meta+shift+c",
+        options: {
+          preventDefault: true,
+        },
       },
     },
     {
